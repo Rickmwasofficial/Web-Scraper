35a4fb95bfb8bca837c6b5341a240c5ec5d74d61
Rickmwasofficial <146796160+Rickmwasofficial@users.noreply.github.com>
2024-04-25 00:00:04 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,13 +13,13 @@ for link in range(len(links)):
 final_list = []
 vote_list = []
 
-
 def get_highest_voted_news(limit):
   for i in range(len(votes)):
     vote_list.append(int(votes[i].contents[0].split(' ')[0]))
   final_list = list(zip(pure_links, vote_list))
   final_list = sorted(final_list, key=lambda x: x[1], reverse=True)
   for y in range(limit):
-    print(f"Link: {final_list[y][0]} Votes: {final_list[y][1]}")
+    print(f"Title: {links[y].getText()} Link: {final_list[y][0]} Votes: {final_list[y][1]}")
 
-get_highest_voted_news(10)
+get_highest_voted_news(20)
+
